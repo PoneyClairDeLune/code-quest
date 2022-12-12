59e87b8e144f95a24eae36624f4bd6e81cf10575
PoneyClairDeLune <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-12-12 17:08:16 +0000

    Tidying the code up a bit.

--- quest/aoc-2022-01/index.c
+++ quest/aoc-2022-01/index.c
@@ -2,11 +2,11 @@
 #include <stdbool.h>
 
 int main() {
-	bool lastBlank = false;
-	bool continueLoop = true;
-	unsigned short calCount = 16384;
-	long calories[16384] = {0};
-	unsigned short calIdx = 0;
+	bool lastBlank = false; // If the last input is blank
+	bool continueLoop = true; // Loop toggle
+	unsigned short calCount = 16384; // So I don't have to write 16384 everywhere...
+	long calories[16384] = {0}; // Stores the list of calories
+	unsigned short calIdx = 0; // The current index
 	// Collect the list of calories
 	do {
 		printf("Calories carried: ");
@@ -21,15 +21,11 @@ int main() {
 			lastBlank = false;
 		};
 		calIdx ++;
+		// Break out of the loop if went out of the bound
 		if (calIdx >= calCount) {
 			continueLoop = false;
 		};
 	} while (continueLoop);
-	// Print the array out for debugging
-	/* for (char i = 0; i < calCount; i ++) {
-		printf("%7i ", calories[i]);
-	}; */
-	// Try to find the elf with the most calories
 	lastBlank = false;
 	continueLoop = true;
 	calIdx = 0;
@@ -42,16 +38,13 @@ int main() {
 		if (thisCal == 0) {
 			if (lastBlank) {
 				continueLoop = false;
-				//printf("Loop exited at %i\n", calIdx);
 			};
 			lastBlank = true;
 			currentCal = 0;
 			currentElf ++;
-			//printf("Next elf!\n");
 		} else {
 			lastBlank = false;
 			currentCal += thisCal;
-			//printf("Elf #%02i has %i calories\n", currentElf + 1, currentCal);
 			if (currentCal > maxCals[0]) {
 				// Just quickly get things done as the assignment demanded...
 				maxCals[2] = maxCals[1];
@@ -60,7 +53,6 @@ int main() {
 				maxElves[2] = maxElves[1];
 				maxElves[1] = maxElves[0];
 				maxElves[0] = currentElf;
-				//printf("Record broke!\n");
 			};
 		};
 		calIdx ++;
@@ -68,6 +60,12 @@ int main() {
 			continueLoop = false;
 		};
 	};
-	printf("Elf #%02i, #%02i and #%02i carries the most calories: %i, %i, %i\n%i calories in total.\n", maxElves[0] + 1, maxElves[1] + 1, maxElves[2] + 1, maxCals[0], maxCals[1], maxCals[2], (maxCals[0] + maxCals[1] + maxCals[2]));
+	printf("These elves have the most calories carried:\n");
+	long long calorySum = 0;
+	for (char i = 0; i < 3; i ++) {
+		printf("#%04i%12i\n", maxElves[i] + 1, maxCals[i]);
+		calorySum += maxCals[i];
+	};
+	printf("Total %11i\n", calorySum);
 	return 0;
 };
