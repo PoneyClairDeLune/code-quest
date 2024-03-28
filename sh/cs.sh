2fe79c51686afe57428fd99c28e3fbc6bf2901f0
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-03-28 09:23:12 +0000

    An alternative approach of quest 03 in C#.

--- sh/cs.sh
+++ sh/cs.sh
@@ -6,6 +6,7 @@ rm -r "./build/$1.dncs"
 echo "Generating temporary project..."
 cp -r "./quest/$1/" "./build/$1.dncs/"
 cp "./common/index.csproj" "./build/$1.dncs/"
+rm -v ./build/$1.dncs/index_*.cs
 echo "Building program..."
 cd "./build/$1.dncs/"
 dotnet build
