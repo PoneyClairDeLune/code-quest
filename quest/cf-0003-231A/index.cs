711affcbfff0a0fe43425acdc5980a78285189a0
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-04-08 19:17:56 +0000

    CodeForces 231A

---
+++ quest/cf-0003-231A/index.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Team {
+	class Program {
+		static byte CountResults(string text) {
+			byte count = 0;
+			for (int i = 0; i < text.Length; i ++) {
+				if (text[i] == '1') {
+					count ++;
+				};
+			};
+			return count;
+		}
+		static void Main(string[] args) {
+			ushort rounds = ushort.Parse(Console.ReadLine());
+			ushort validRuns = 0;
+			for (ushort round = 0; round < rounds; round ++) {
+				if (CountResults(Console.ReadLine()) > 1) {
+					validRuns ++;
+				};
+			};
+			Console.WriteLine(validRuns.ToString());
+		}
+	}
+}
