85a69c031631f405c5087226c70e35cd3523e86c
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-12-05 19:33:48 +0000

    Failed AoC 2024 Day 4 Part 2.

--- quest/aoc-2024-04/index.js
+++ quest/aoc-2024-04/index.js
@@ -11,8 +11,15 @@ let parseLine = async (targetArray, readable) => {
 		targetArray.push(line); // Assumes ASCII/UTF-8
 	};
 };
+// The "XMAS" matcher
 const xmasBinData = [88, 77, 65, 83];
-let matchXmas = (bin2d, posX, posY, direction) => {
+const preRotMasBin = [
+	[77, 83, 65, 77, 83], // L
+	[83, 83, 65, 77, 77], // B
+	[83, 77, 65, 83, 77], // R
+	[77, 77, 65, 83, 83]  // T
+]; // Fine, I'll rotate it myself
+let matchXmasFull = (bin2d, posX, posY, direction) => {
 	/*
 	Direction parameter (3-bit):
 	L -> R
@@ -102,6 +109,38 @@ let matchXmas = (bin2d, posX, posY, direction) => {
 	};
 	return matchCount == xmasBinData.length;
 };
+// The "MMASS" matcher
+let matchXmasMas = (bin2d, posX, posY, direction) => {
+	/*
+	Direction:
+	0: L->R
+	1: B->T
+	2: R->L
+	3: T->B
+	*/
+	let sWidth = bin2d[0].length, sHeight = bin2d.length;
+	// Error out if the match is attempting to access OOB
+	// Centre index match
+	if (posX < 1 || posY < 1 || posX + 2 > sWidth || posY + 2 > sHeight) {
+		return false;
+	};
+	let extractedView = new Uint8Array(5);
+	extractedView[0] = bin2d[posY - 1][posX - 1];
+	extractedView[1] = bin2d[posY - 1][posX + 1];
+	extractedView[2] = bin2d[posY][posX];
+	extractedView[3] = bin2d[posY + 1][posX - 1];
+	extractedView[4] = bin2d[posY + 1][posX + 1];
+	let matchCount = 0;
+	for (let i = 0; i < preRotMasBin[direction].length; i ++) {
+		if (extractedView[i] == preRotMasBin[direction][i]) {
+			matchCount ++;
+		};
+	};
+	if (matchCount == preRotMasBin[direction].length) {
+		//console.debug(extractedView);
+	};
+	return matchCount == preRotMasBin[direction].length;
+};
 
 // Test section
 /*let testW = 5, testH = 5, testD = 4;
@@ -111,31 +150,62 @@ for (let y = 0; y < testH; y ++) {
 };
 for (let y = 0; y < testH; y ++) {
 	for (let x = 0; x < testW; x ++) {
-		console.debug(`OOB yet at (${x}, ${y}) ${testD}? ${matchXmas(testData, x, y, testD)}`);
+		console.debug(`OOB yet at (${x}, ${y}) ${testD}? ${matchXmasMas(testData, x, y, testD)}`);
 	};
 };*/
 
 // Import and pre-process input
 let flatData = [];
 await parseLine(flatData, (await Deno.open("./dataInput.txt")).readable);
-let firstOccurs = [];
+
+// Part 1: Count linear XMAS
+
+let firstOccursA = [];
 for (let y = 0; y < flatData.length; y ++) {
 	for (let x = 0; x < flatData[0].length; x ++) {
-		if (flatData[y][x] == 88) {
-			firstOccurs.push([x, y]);
+		if (flatData[y][x] == 88) { // X
+			firstOccursA.push([x, y]);
 		};
 	};
 };
-console.debug(`The letter "X" has occurred ${firstOccurs.length} times.`);
+console.debug(`The letter "X" has occurred ${firstOccursA.length} times.`);
 let part1Result = 0;
-for (let occuredX of firstOccurs) {
+for (let occuredX of firstOccursA) {
 	for (let direction = 0; direction < 8; direction ++) {
-		let matchResult = matchXmas(flatData, ...occuredX, direction);
+		let matchResult = matchXmasFull(flatData, ...occuredX, direction);
 		//console.debug(`(${occuredX.join(", ")}) ${direction}: ${matchResult}`);
 		if (matchResult) {
 			part1Result ++;
-			console.debug(`(${occuredX.join(", ")}) ${direction}: ${matchResult}`);
+			//console.debug(`(${occuredX.join(", ")}) ${direction}: ${matchResult}`);
 		};
 	};
 };
 console.debug(`Part #1: ${part1Result}`);
+
+// Part 2: Count 2D X-shaped MAS
+
+let firstOccursB = [];
+for (let y = 0; y < flatData.length; y ++) {
+	for (let x = 0; x < flatData[0].length; x ++) {
+		if (flatData[y][x] == 65) { // A
+			firstOccursB.push([x, y]);
+		};
+	};
+};
+console.debug(`The letter "A" has occurred ${firstOccursB.length} times.`);
+let part2Result = 0;
+for (let occuredA of firstOccursB) {
+	let solved = false;
+	for (let direction = 0; direction < 4; direction ++) {
+		if (solved) {
+			continue;
+		};
+		let matchResult = matchXmasMas(flatData, ...occuredA, direction);
+		//console.debug(`(${occuredA.join(", ")}) ${direction}: ${matchResult}`);
+		if (matchResult) {
+			part1Result ++;
+			//console.debug(`(${occuredA.join(", ")}) ${direction}: ${matchResult}`);
+		};
+	};
+};
+console.debug(`Part #2: ${part1Result}`);
