dfe768274d056a2650735e5d9239d0b0dbd32786
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-12-05 18:37:22 +0000

    Failed AoC 2024 Day 4 Part 1.

---
+++ quest/aoc-2024-04/index.js
@@ -0,0 +1,133 @@
+"use strict";
+
+// Required preparation steps
+
+import TextReader from "https://jsr.io/@ltgc/rochelle/0.2.2/dist/textRead.mjs";
+
+const debugMode = 4;
+let parseLine = async (targetArray, readable) => {
+	let lineReader = TextReader.lineRaw(readable);
+	for await (let line of lineReader) {
+		targetArray.push(line); // Assumes ASCII/UTF-8
+	};
+};
+const xmasBinData = [88, 77, 65, 83];
+let matchXmas = (bin2d, posX, posY, direction) => {
+	/*
+	Direction parameter (3-bit):
+	L -> R
+	T -> B
+	TL -> BR
+	BL -> TR
+	---
+	inverted?
+	*/
+	let sWidth = bin2d[0].length, sHeight = bin2d.length;
+	// Error out if the match is attempting to access OOB
+	// Start index match
+	if (posX < 0 || posY < 0 || posX >= sWidth || posY >= sHeight) {
+		return false;
+	};
+	// End index match
+	//console.debug([posX, sWidth - posX - 1][direction & 1] + xmasBinData.length);
+	switch (direction >> 1) {
+		case 0: {
+			if ([posX, sWidth - posX - 1][direction & 1] + xmasBinData.length > sWidth) {
+				return false;
+			};
+			break;
+		};
+		case 1: {
+			if ([posY, sHeight - posY - 1][direction & 1] + xmasBinData.length > sHeight) {
+				return false;
+			};
+			break;
+		};
+		case 2: {
+			if ([posX, sWidth - posX - 1][direction & 1] + xmasBinData.length > sWidth || [posY, sHeight - posY - 1][direction & 1] + xmasBinData.length > sHeight) {
+				return false;
+			};
+			break;
+		};
+		case 3: {
+			if ([posX, sWidth - posX - 1][direction & 1] + xmasBinData.length > sWidth || [sHeight - posY - 1, posY][direction & 1] + (xmasBinData.length ^ 1) > sHeight) {
+				return false;
+			};
+			break;
+		};
+	};
+	let matchCount = 0;
+	for (let i = 0; i < xmasBinData.length; i ++) {
+		// Coordinate translation
+		let rX = posX, rY = posY;
+		switch (direction) {
+			case 0:
+			case 4:
+			case 6: {
+				rX += i;
+				break;
+			};
+			case 1:
+			case 5:
+			case 7: {
+				rX -= i;
+				break;
+			};
+		}
+		switch (direction) {
+			case 2:
+			case 4:
+			case 7: {
+				rY += i;
+				break;
+			};
+			case 3:
+			case 5:
+			case 6: {
+				rY -= i;
+				break;
+			};
+		}
+		if (bin2d[rY][rX] == xmasBinData[i]) {
+			matchCount ++;
+		};
+	};
+	return matchCount == xmasBinData.length;
+};
+
+// Test section
+/*let testW = 5, testH = 5, testD = 4;
+let testData = [];
+for (let y = 0; y < testH; y ++) {
+	testData.push(new Array(testW).fill(0));
+};
+for (let y = 0; y < testH; y ++) {
+	for (let x = 0; x < testW; x ++) {
+		console.debug(`OOB yet at (${x}, ${y}) ${testD}? ${matchXmas(testData, x, y, testD)}`);
+	};
+};*/
+
+// Import and pre-process input
+let flatData = [];
+await parseLine(flatData, (await Deno.open("./dataInput.txt")).readable);
+let firstOccurs = [];
+for (let y = 0; y < flatData.length; y ++) {
+	for (let x = 0; x < flatData[0].length; x ++) {
+		if (flatData[y][x] == 88) {
+			firstOccurs.push([x, y]);
+		};
+	};
+};
+console.debug(firstOccurs);
+let part1Result = 0;
+for (let occuredX of firstOccurs) {
+	for (let direction = 0; direction < 8; direction ++) {
+		let matchResult = matchXmas(flatData, ...occuredX, direction);
+		//console.debug(`(${occuredX.join(", ")}) ${direction}: ${matchResult}`);
+		if (matchResult) {
+			part1Result ++;
+			console.debug(`(${occuredX.join(", ")}) ${direction}: ${matchResult}`);
+		};
+	};
+};
+console.debug(`Part #1: ${part1Result}`);
