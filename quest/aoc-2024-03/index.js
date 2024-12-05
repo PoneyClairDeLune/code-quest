f4dbc2c902421af22e3d7dacf3bc6e3dcead3edf
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-12-05 17:20:48 +0000

    AoC 2024 Day 3 Part 2.

--- quest/aoc-2024-03/index.js
+++ quest/aoc-2024-03/index.js
@@ -1,15 +1,19 @@
 "use strict";
 
 // Required preparation steps
-let parseCommandSimple = (cmd) => {
+
+let parseCommand = (cmd) => {
 	/*
 	0: waiting for (
 	1: waiting for ,
 	2: waiting for )
 	*/
-	let command = cmd.match(/[A-Za-z0-9_]+\(/)[0];
-	let args = cmd.match(/\([0-9,]+\)/)[0];
-	args = args.substring(1, args.length - 1).split(",");
+	let command = cmd.match(/[A-Za-z0-9_']+\(/)[0];
+	//console.debug(command);
+	let args = (cmd.match(/\([0-9,]+\)/) ?? [])[0] || [];
+	if (args?.length) {
+		args = args.substring(1, args.length - 1).split(",");
+	};
 	return [
 		command.substring(0, command.length - 1),
 		...args
@@ -20,17 +24,24 @@ let parseCommandSimple = (cmd) => {
 
 let inputData = await Deno.readTextFile("./dataInput.txt");
 let commandChain = [];
-for (let e of inputData.match(/mul\(\d+,\d+\)/g)) {
-	commandChain.push(parseCommandSimple(e));
+for (let e of inputData.match(/(mul|do(|n't))\((\d+,\d+|)\)/g)) {
+	let command = parseCommand(e);
+	commandChain.push(command);
+	console.debug(command);
 };
-console.debug(commandChain);
 
 // Part 1: Sum of all simply-matched multiplication
+
 let sumSimple = 0;
 for (let command of commandChain) {
 	switch (command[0]) {
 		case "mul": {
 			sumSimple += parseInt(command[1]) * parseInt(command[2]);
+			break;
+		};
+		case "do":
+		case "don't": {
+			// Ignored commands
 			break;
 		};
 		default: {
@@ -39,3 +50,29 @@ for (let command of commandChain) {
 	};
 };
 console.info(`Part #1: ${sumSimple}`);
+
+// Part 2: Sum of all valid multiplications
+
+let sumToggled = 0, isMultiplying = true;
+for (let command of commandChain) {
+	switch (command[0]) {
+		case "don't": {
+			isMultiplying = false;
+			break;
+		};
+		case "do": {
+			isMultiplying = true;
+			break;
+		};
+		case "mul": {
+			if (isMultiplying) {
+				sumToggled += parseInt(command[1]) * parseInt(command[2]);
+			};
+			break;
+		};
+		default: {
+			console.debug(`Unknown command parsed: "${command[0]}"`);
+		};
+	};
+};
+console.info(`Part #2: ${sumToggled}`);
