03ca18992a97b0a53abe060d832ebeec857ccd6f
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-03-14 13:26:49 +0000

    Oooooo elbow GREASE!

--- quest/test-random-string/index.go
+++ quest/test-random-string/index.go
@@ -5,10 +5,19 @@ import (
 	"math/rand"
 )
 
-func getChManglingChar() string {
-	return string(" ,-_:;()"[rand.Int()&7]);
+//func getChManglingChar() string {
+	//return string(" ,-_:;()"[rand.Int()&7]);
+//}
+
+var clientHintGreaseNA = []string{" ", "(", ":", "-", ".", "/", ")", ";", "=", "?", "_"}
+var clientHintVersionNA = []string{"8", "99", "24"}
+
+func getGreasedChInvalidBrand(seed int) string {
+	return "\"Not" + clientHintGreaseNA[seed % len(clientHintGreaseNA)] + "A" + clientHintGreaseNA[(seed + 1) % len(clientHintGreaseNA)] + "Brand\";v=\"" + clientHintVersionNA[seed % len(clientHintVersionNA)] + "\"";
 }
 
 func main() {
-	fmt.Println("Some random characters: " + getChManglingChar());
+	//fmt.Println("Some random characters: " + getChManglingChar());
+	fmt.Println("Greased string:", getGreasedChInvalidBrand(145))
+	fmt.Println("Greased string:", getGreasedChInvalidBrand(rand.Int()))
 }
