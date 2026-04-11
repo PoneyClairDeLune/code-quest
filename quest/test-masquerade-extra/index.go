5b32b90d1447fc1be3dd8d2b2222f015aa7d642d
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-04-11 19:09:14 +0000

    Typo correction.

--- quest/test-masquerade-extra/index.go
+++ quest/test-masquerade-extra/index.go
@@ -13,7 +13,7 @@ func GetCurlVersion() string {
 	// curl 8.0.0 was released on 20/03/2023.
 	var timeCurrent int64 = time.Now().Unix() / 86400
 	var timeStart int64 = time.Date(2023, 3, 20, 0, 0, 0, 0, time.UTC).Unix() / 86400
-	// Generate some variations upon the average release cadance of 56.67 days.
+	// Generate some variations upon the average release cadence of 56.67 days.
 	var timeDiff int = int((timeCurrent - timeStart - 60)) - int(math.Floor(math.Pow(rand.Float64(), 2) * 165))
 	var minorValue int = int(timeDiff / 57)
 	return "8." + strconv.Itoa(minorValue) + ".0"
@@ -22,7 +22,7 @@ func GetFirefoxVersion() string {
 	// Firefox 128 ESR was released on 09/07/2023.
 	var timeCurrent int64 = time.Now().Unix() / 86400
 	var timeStart int64 = time.Date(2024, 7, 29, 0, 0, 0, 0, time.UTC).Unix() / 86400
-	// Generate some variations upon the average release cadance of 28 days, however Firefox had some delays averaging to 29.67 days in total.
+	// Generate some variations upon the average release cadence of 28 days, however Firefox had some delays averaging to 29.67 days in total.
 	var timeDiff = timeCurrent - timeStart - 15 - int64(math.Floor(math.Pow(rand.Float64(), 2) * 45))
 	var majorValue int = int(timeDiff / 29) + 128
 	return strconv.Itoa(majorValue) + ".0"
